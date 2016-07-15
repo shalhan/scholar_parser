fa4a3d4acc4b142319bb648c4b6b045855cbcc0a
shalhan <shalhanradifan44@gmail.com>
2016-07-15 15:57:50 +0700

    insert  &  to array

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 // then click the URL that is emitted to the Output tab of the console
 
 // get host name from URL
+$USER_DATA = array();
 $EXTRACTED_PAPER_DATA = array();
 $EXTRACTED_SUMMARY_DATA = array();
 
@@ -53,12 +54,36 @@ $tableRows = $matches[0];
 foreach($tableRows as $row){
 
     //mengambil citedby, tahun
-    $left=regexForm('<td');
+    $left=regexForm('<td ');
     $right= regexForm('</td>');
     $regex = "/$left(.*?)$right/s";
     $matches = array();
     preg_match_all($regex, $row, $matches);
-    $detail = array('makalah' => $matches[0][0], 'cited' => str_replace("*","",$matches[0][1]), 'year' => $matches[0][2]);
+
+    //memisah paper detail menjadi judul, author & journal
+        //judul paper
+    $paperDetail= $matches[0][0];
+    $left=regexForm('<a ');
+    $right= regexForm('</a>');
+    $regex = "/$left(.*?)$right/s";
+    $paperJudul = array();
+    preg_match_all($regex, $paperDetail, $paperJudul);
+
+        //author & journal
+    $paperDetail= $matches[0][0];
+    $left=regexForm('<div class="gs_gray">');
+    $right= regexForm('</div>');
+    $regex = "/$left(.*?)$right/s";
+    $paperAJ = array();
+    preg_match_all($regex, $paperDetail, $paperAJ);
+
+    $detail = array(
+        'judul' => $paperJudul[0][0],
+        'author' => $paperAJ[1][0],
+        'journal' => $paperAJ[1][1],
+        'cited' => str_replace("*","",$matches[0][1]),
+        'year' => $matches[0][2]
+    );
     $EXTRACTED_PAPER_DATA[] = $detail;
 }
 
@@ -86,28 +111,35 @@ $right= regexForm('</td>');
 $regex = "/$left(.*?)$right/s";
 $matches = array();
 preg_match_all($regex, $getDetail, $matches);
-$userDetail = array('citation' => $matches[0][0], 'hindex' => $matches[0][2]);
+$userDetail = array(
+        'citation' => $matches[0][0],
+        'hindex' => $matches[0][2]
+    );
 $EXTRACTED_SUMMARY_DATA = $userDetail;
 
-    //get nama
+    //get nama & total paper
 $left= regexForm('<div id="gsc_prf_in">');
 $right = regexForm('</div>');
 $regex = "/$left(.*?)$right/s";
 $matches = array();
 preg_match_all($regex, $data, $matches);
 
-$userNama = $matches[1][0];
-
+$userName = $matches[1][0];
 $totalPaper = sizeof($EXTRACTED_PAPER_DATA);
 
+$EXTRACTED_PAPER_DATA['name'] = $userName;
+$EXTRACTED_PAPER_DATA['total_paper'] = $totalPaper;
+
 //PRINT
-echo "Nama : " . $userNama . "<br>";
+echo "Nama : " . $EXTRACTED_PAPER_DATA[name] . "<br>";
 echo "Citation : " . $EXTRACTED_SUMMARY_DATA[citation] . "<br>";
 echo "H-Index : " . $EXTRACTED_SUMMARY_DATA[hindex] . "<br>";
-echo "Total Paper : " . $totalPaper . "<br><br>";
+echo "Total Paper : " . $EXTRACTED_PAPER_DATA[total_paper] . "<br><br>";
 
 for($i=0; $i<$totalPaper; $i++){
-    echo "Paper : " . $EXTRACTED_PAPER_DATA[$i][makalah];
+    echo "Judul Paper : " . $EXTRACTED_PAPER_DATA[$i][judul] . "<br>";
+    echo "Author : " . $EXTRACTED_PAPER_DATA[$i][author] . "<br>";
+    echo "Journal : " . $EXTRACTED_PAPER_DATA[$i][journal] . "<br>";
     echo "Citedby : " . $EXTRACTED_PAPER_DATA[$i][cited] . "<br>";
     echo "Year : " . $EXTRACTED_PAPER_DATA[$i][year] . "<br><br>";
 }
